e35c66d9ceca56c842712ac384c1d992fdb6d382
Alexwont <alex1ae1kd@gmail.com>
2026-06-23 21:11:10 -0500

    Actualizando diseño de SOAD y configuraciones de despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Web_Page_1/', // <--- CAMBIA ESTO por el nombre exacto de tu repo
 })
